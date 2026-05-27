90af8739f65cfb248ae8942819c67e5634237692
kietnormal <vituankietdzvcl2007@gmail.com>
2026-05-28 05:30:05 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -132,3 +132,9 @@ function showPage1Instantly() {
     document.body.classList.remove("reading");
 }
 
+
+
+document.querySelector(".action-menu > button").addEventListener("click", 
+    function() {
+        document.querySelector(".action-menu").classList.toggle("active");
+    });
